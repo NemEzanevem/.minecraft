af9356409131a7927f8ccb47539b754a0b31b748
NemEzanevem <99277804+NemEzanevem@users.noreply.github.com>
2024-08-12 15:13:35 +0200

    fix

--- kubejs/server_scripts/bloodmagic/more_gating.js
+++ kubejs/server_scripts/bloodmagic/more_gating.js
@@ -11,3 +11,14 @@ ServerEvents.recipes(event => {
 })
 
 
+ServerEvents.recipes(event => {
+    function BloodAltarRecipes(Output, Input, Level, ConsumptionRate, DrainRate){
+    event.recipes.bloodmagic.altar(Output, Input).upgradeLevel(Level).consumptionRate(ConsumptionRate).drainRate(DrainRate)
+    }
+    BloodAltarRecipes('1x bloodmagic:blankslate', 'pneumaticcraft:compressed_stone', 0, 1000, 10)
+})
+
+//removals
+ServerEvents.recipes(event => {
+    event.remove({ id: 'bloodmagic:altar/slate' })
+})
